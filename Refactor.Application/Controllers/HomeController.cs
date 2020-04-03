7a4ce7a6acf50f58eb10b554b1c73f55cae36ef1
nick3434 <nick3434@gmail.com>
2020-04-03 15:35:46 +0800

    Init

---
+++ Refactor.Application/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Refactor.Service;
+using Refactor.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Refactor.Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        IPOIService _POIService;
+        public HomeController(IPOIService poiService)
+        {
+            //POIService改成依賴介面IPOIService
+            //並且實體是由外部來決定，也就是所謂的依賴注入DI
+            _POIService = poiService;
+        }
+
+
+        public ActionResult Index(string id)
+        {
+            //依賴外部注入的IPOIService介面
+            var POIs = _POIService.Get(id);
+            return Json(POIs,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Refactor.Service/POIService.cs
@@ -0,0 +1,46 @@
+﻿using Lib.Refactor;
+using Lib.Refactor.Enums;
+using Refactor.Repository;
+using Refactor.Repository.Factorys;
+using Refactor.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor.Service
+{
+    public class POIService :IPOIService
+    {
+        IChannelService _ChannelService;
+        public POIService(IChannelService channelService)
+        {
+            //DI
+            _ChannelService = channelService;
+        }
+
+        public List<POI> Get(string channelID)
+        {
+            var Result = new List<POI>();
+
+            //面對抽象
+            var channel = _ChannelService.Get(channelID);
+
+            //逐一搜尋頻道底下的各類別
+            foreach (var category in channel.Categorys)
+            {
+                //依據該類別所指定的供應商向Repository要資料
+                category.Supplier.ForEach(x =>
+                {
+                    var Repository = SupplierFactory.Generate(x);
+                    var POIs = Repository.Get();
+                    Result.AddRange(POIs);
+                });
+            }
+
+            //並接總合的結果回傳
+            return Result;
+        }
+    }
+}
